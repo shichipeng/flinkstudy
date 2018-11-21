d4d5bab7e1f62e3f9cac65f9c39375d382949280
shichipeng <shichipengbs@163.com>
2018-11-21 12:47:25 +0800

    'test1'

--- src/main/scala/com/meituan/flink/java/Test1.java
+++ src/main/scala/com/meituan/flink/java/Test1.java
@@ -33,8 +33,8 @@ public class Test1 {
         env.execute("window wordCount");
 
 
-        System.out.println("my first use it,i like do it");
 
+        System.out.println("my first use it,i like do it,haha");
     }
 
 
